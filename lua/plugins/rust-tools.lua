2625dbd1fbf5ef0cb2b2c0342348dbfb9fdeba5b
Sahuagin <sahuagin@users.noreply.github.com>
2023-10-01 14:43:42 -0400

    Better integration with the language server.

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -5,5 +5,19 @@ return {
 		-- for debugging
 		"nvim-lua/plenary.nvim",
 		"mfussenegger/nvim-dap",
+		config = function()
+			local rt = require("rust-tools")
+
+			rt.setup({
+				server = {
+					on_attach = function(_, bufnr)
+						-- hover actions
+						vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+						-- Code action groups
+						vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+					end,
+				},
+			})
+		end,
 	},
 }
